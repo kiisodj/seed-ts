2a5770c5affe9725e7795a8ca7330a0cbcd5456d
Simon Neaves <simon@aerian.com>
2019-11-29 20:41:33 +0000

    get jest working - still having a problem with linting json files

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
+import { sum } from "./index";
+
 describe("test suite", () => {
     it("runs the test suite", () => {
         expect(true).toBeTruthy();
     });
 });
+
 describe("imported function", () => {
     it("can test an imported function", () => {
-        const four = index_1.sum(1, 3);
+        const four = sum(1, 3);
         expect(four).toEqual(4);
         expect(four).not.toEqual(5);
     });
--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import path from "path";
 
-export const sum = (a: number, b: number) => {
-    return a + b;
-};
+export const sum = (a: number, b: number) => a + b;
 
 console.log(path.resolve(__dirname, "../"));
